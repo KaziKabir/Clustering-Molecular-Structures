9a2a050ebc9cc85a5518c9f2185b9b824f3f5272
KaziKabir <42122566+KaziKabir@users.noreply.github.com>
2020-06-16 17:12:12 -0400

    Update extract_CA.py

--- extract_CA.py
+++ extract_CA.py
@@ -7,7 +7,7 @@ with open('1dtja_models_5000.pdb') as f: #pdb file having the tertiary structure
 		words = line.split()
 		if words[0] == 'MODEL':
 			out.write('222') # values for the CA coordinates per structure/model (74*3)
-		if len(words) > 2 and words[2] == 'CA' and int(words[1]) < 1155: # line with CA coorniates
+		if len(words) > 2 and words[2] == 'CA': # line with CA coorniates
 			out.write(' ' + words[6] + ' ' + words[7] + ' ' + words[8])
 		if ('TER' or 'ENDMDL') in words:
 			out.write('\n')
